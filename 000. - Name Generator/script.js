0757bcd112a31d671b00f85456cc658c7e988f76
Sebastian <pocztasebastian1306@gmail.com>
2025-07-01 18:15:04 +0200

    htm, css, JS niekompletne

---
+++ 000. - Name Generator/script.js
@@ -0,0 +1,18 @@
+var generateButton = document.querySelector("#generate-button");
+var label = document.querySelector("#label");
+
+label.textContent = document.querySelector(".select-list")[0].textContent;
+
+// generateButton.onclick = function(){
+//     label.textContent =  ;
+//     // w zalezności od wyboruu w select umiesc albo 1  WYLOSOWANE żeńskie, albo męskie
+// };
+
+// switch(true){
+//     case "man":
+//         //wylosuj 1 imie z listy imion męskich
+//     case "woman":
+//         //wylosuj 1 imię z imoion rzeńskich
+//     case "woman and man":
+//         //wylosuj 1 imię z obu list
+// }
